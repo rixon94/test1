22a542c3957adc2048275fbd695aedd9f57a90bd
rixon94 <44235942+rixon94@users.noreply.github.com>
2025-02-10 10:15:29 +0100

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,9 +1,11 @@
 class Sample{
 
 public static void main(String[] arg){
+System.out.println("Hi Welcome123");
 System.out.println("Hi Welcome");
 System.out.println("Hi Welcome");
 System.out.println("Hi Welcome");
-System.out.println("Hi Welcome");
+  System.out.println("Hi Welcome");
+
 }
 }
